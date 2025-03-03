7be421ffd1294f054631bcba8051150c0b305c16
ChemaArmenta26 <159948311+ChemaArmenta26@users.noreply.github.com>
2025-03-02 17:28:18 -0700

    Maquetación de 3 pantallas
    Pantalla de inicio, detalle gasto y codigo del grupo

---
+++ ProyectoFinal_TripSplit/app/src/main/java/armenta/jose/proyectofinal_tripsplit/InicioActivity.kt
@@ -0,0 +1,22 @@
+package armenta.jose.proyectofinal_tripsplit
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class InicioActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_inicio)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ ProyectoFinal_TripSplit/app/src/main/java/armenta/jose/proyectofinal_tripsplit/ui/fragments/TopBarFragment.kt
@@ -0,0 +1,17 @@
+package armenta.jose.proyectofinal_tripsplit.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import armenta.jose.proyectofinal_tripsplit.R
+
+class TopBarFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_top_bar, container, false)
+    }
+}
