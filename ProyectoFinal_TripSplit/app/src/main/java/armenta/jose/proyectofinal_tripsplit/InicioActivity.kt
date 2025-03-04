fd8b315a692aa022914d838ab6f4ab36711d2928
ChemaArmenta26 <159948311+ChemaArmenta26@users.noreply.github.com>
2025-03-03 21:12:35 -0700

    Corrección con problemas del SDK Y JDK

--- ProyectoFinal_TripSplit/app/src/main/java/armenta/jose/proyectofinal_tripsplit/InicioActivity.kt
+++ ProyectoFinal_TripSplit/app/src/main/java/armenta/jose/proyectofinal_tripsplit/InicioActivity.kt
@@ -1,7 +1,9 @@
 package armenta.jose.proyectofinal_tripsplit
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,10 +15,14 @@ class InicioActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_inicio)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        val btnUnete = findViewById<Button>(R.id.btn_ingresar)
+
+        btnUnete.setOnClickListener {
+            val intent = Intent(this, CrearCuentaActivity::class.java)
+
+            startActivity(intent)
         }
     }
 }
