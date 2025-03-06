5f3b0bfd4b92bbf6945b424f909570ef6353c75d
ChemaArmenta26 <159948311+ChemaArmenta26@users.noreply.github.com>
2025-03-05 18:19:43 -0700

    Corrección de un fragmento

--- ProyectoFinal_TripSplit/app/build/generated/data_binding_base_class_source_out/debug/out/armenta/jose/proyectofinal_tripsplit/databinding/ActivityEditarGrupoBinding.java
+++ ProyectoFinal_TripSplit/app/build/generated/data_binding_base_class_source_out/debug/out/armenta/jose/proyectofinal_tripsplit/databinding/ActivityEditarGrupoBinding.java
@@ -7,17 +7,30 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewbinding.ViewBinding;
+import androidx.viewbinding.ViewBindings;
 import armenta.jose.proyectofinal_tripsplit.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 
 public final class ActivityEditarGrupoBinding implements ViewBinding {
   @NonNull
   private final LinearLayout rootView;
 
-  private ActivityEditarGrupoBinding(@NonNull LinearLayout rootView) {
+  @NonNull
+  public final BottomNavigationView navView;
+
+  @NonNull
+  public final RecyclerView rvIntegrantes;
+
+  private ActivityEditarGrupoBinding(@NonNull LinearLayout rootView,
+      @NonNull BottomNavigationView navView, @NonNull RecyclerView rvIntegrantes) {
     this.rootView = rootView;
+    this.navView = navView;
+    this.rvIntegrantes = rvIntegrantes;
   }
 
   @Override
@@ -43,10 +56,25 @@ public final class ActivityEditarGrupoBinding implements ViewBinding {
 
   @NonNull
   public static ActivityEditarGrupoBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.nav_view;
+      BottomNavigationView navView = ViewBindings.findChildViewById(rootView, id);
+      if (navView == null) {
+        break missingId;
+      }
 
-    return new ActivityEditarGrupoBinding((LinearLayout) rootView);
+      id = R.id.rv_integrantes;
+      RecyclerView rvIntegrantes = ViewBindings.findChildViewById(rootView, id);
+      if (rvIntegrantes == null) {
+        break missingId;
+      }
+
+      return new ActivityEditarGrupoBinding((LinearLayout) rootView, navView, rvIntegrantes);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
